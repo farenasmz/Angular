501402f8d91ae7a81e88e8c9968278874cdf31b0
Fabian <siempreunvacan1@hotmail.com>
2019-08-04 19:44:40 -0500

    Modulo 1 finalizado

--- Proyectos angular/Angular Modulo 1/my-app/src/app/app.module.ts
+++ Proyectos angular/Angular Modulo 1/my-app/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { BodyComponent } from './Components/Body/Body.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
